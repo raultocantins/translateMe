41d2fa22696f0d834598e59196a981f03a224482
Alex raul <alexbraul.ar@gmail.com>
2020-11-05 22:47:19 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      app
+      color="primary"
+      dark
+    class="appbar"    
+    >
+    <h1>TranslateMe</h1>
+    </v-app-bar>
+
+    <v-main fluid>
+    <Content/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Content from './components/Content'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  components: {
+    Content,
+  },
+  
+
+  data: () => ({
+    //
+    
+  }),
+};
+</script>
+<style scoped>
+.appbar{
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-content>
+    <v-toolbar light class="mr-5 ml-5 pb-5"  src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+      <v-select :items="items" :label="items[0]" solo class="mr-4 "/> 
+      <v-select :items="items" :label="items[1]" solo></v-select>
+    </v-toolbar>
+
+    <v-col cols="12"  class="content">
+      <v-col md="6" xs="12">
+        <v-textarea
+        
+          outlined
+          name="input-7-4"
+          label="Outlined textarea"
+          value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+        ></v-textarea>
+      </v-col>
+      <v-col md="6" xs="12">
+        <v-textarea
+          outlined
+          name="input-7-4"
+          label="Outlined textarea"
+          value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+        ></v-textarea>
+      </v-col>
+    </v-col>
+  </v-content>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      items: ["PT", "EN"],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.content{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
